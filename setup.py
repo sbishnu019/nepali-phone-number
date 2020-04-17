edd56f1ebc63c9fd997eed75f3237e26e3117741
Bishnu <sbishnu019@gmail.com>
2020-04-17 10:28:07 +0545

    set download_ulr

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sbishnu019/nepali-phone-number",
+    download_url='https://github.com/sbishnu019/nepali-phone-number/archive/v0.0.1.tar.gz',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
